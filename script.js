284dbbc808e2a770c090a948b0fd7fe79d0f1e86
sguattero <sguattero516@mail.ru>
2020-02-07 13:59:19 +0300

    bem

---
+++ script.js
@@ -0,0 +1,135 @@
+"use strict"
+// let nameUser=prompt("What is your name?");
+// alert(`Your name ${nameUser}`)
+
+/*let nameProgram = prompt(`Какое "официальное" название JavaScript?`);
+if(nameProgram == "ECMAscript"){
+    alert("False!")
+}
+else {
+    alert(`You don't now? "ECMAscript`);
+}*/
+
+/*let comparison = prompt("Insert the number");
+if(comparison>0){
+    alert("1");
+}
+else if(comparison<0){
+    alert("-1");
+}
+else if(comparison==0){
+    alert("0");
+}*/
+
+/*let indefin = prompt("Who's there?", '');
+if(indefin=="Admin"){
+let pass = prompt("Enter Password", '');
+
+if(pass == "i'm Boss"){
+    alert("hello");
+}
+else if(pass == ""|| pass == null){
+    alert("cancellation");
+}
+else{
+    alert("Wrong password");
+}
+}
+else if(indefin==""||indefin==null){
+    alert("cancellation");
+}
+else{
+    alert("i don't know you")
+}*/
+
+/*let cycle;
+do{
+    cycle = prompt("Enter a number great than 100", 0);
+    if(cycle>100){
+        alert("False");
+    }
+}
+while(cycle<=100||null);*/
+
+/*let browser = prompt("Enter your browser");
+if(browser == "Edge"){
+    alert("You've got the Edge");
+}
+else if(browser == "Chrome"||"Firefox"||"Safari"||"Opera"){
+    alert("okay we support these browsers too");
+}
+else{
+    alert("We hope that this page lookc ok!");
+}*/
+
+/*const number = +prompt('Enter a number between 0 and 3');
+switch(number){
+    case 0:
+        alert('You entered the number 0');
+        break;
+    
+    case 1:
+        alert('You entered the number 1');
+        break;
+    case 2:
+        alert('You entered the number 2');
+        break; 
+    default:
+        alert('You losse');
+}*/
+
+
+/*function min(a,b){
+    if (a<b){
+        return a;
+    }   else{
+        return b;
+    }
+}
+
+alert(min(2,5))*/
+
+
+
+/*function pow(x,n){
+   let result = x;
+   for(let i=1;i<=n;i++){
+       result *= n;
+       
+   }
+       return result;
+}
+
+let x = prompt('x?');
+let n = prompt('n?');
+
+alert(pow(x,n));*/
+
+
+/*let user = {};
+user.name ='John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+user.age = 25;
+for(let key in user){
+document.write(user[key]);
+}
+
+
+let salaries = {
+    John:100,
+    Ann:160,
+    Pete:130
+}
+
+let sum = (salaries.John + salaries.Ann + salaries.Pete);
+
+alert(sum);*/
+
+
+
+
+
+
+
